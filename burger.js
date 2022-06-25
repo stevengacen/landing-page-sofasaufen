0ed1b20ec0ee57bbe36785f0ba0436891551e586
DirtyClean1 <101983082+DirtyClean1@users.noreply.github.com>
2022-06-25 14:50:01 +0200

    Update burger.js

--- burger.js
+++ burger.js
@@ -5,31 +5,21 @@ let radio = document.getElementById("toggle");
 
 window.onresize = function() {
     if (window.innerWidth <= 850) {
-
-
     } else {
         burgermenu.style.display = "none";
     }
-
 }
 
 burger.onclick = function() {
-
     if (burgermenu.style.display == "none") {
         burgermenu.style.display = "block";
-
-
     } else {
         burgermenu.style.display = "none";
     }
-
-
-
     for (let i = 0; i < burgerlinks.length; i++) {
         burgerlinks[i].onclick = function() {
             burgermenu.style.display = "none";
             radio.checked = false;
         }
-
     }
 }
